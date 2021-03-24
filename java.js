c821208042754074a4af00bbf50497efa511ed54
Alexey <alexnozdrin23@gmail.com>
2021-03-24 20:07:45 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,28 @@
+var message='Учимся 2'
+console.log(message);
+// let sayHello='Hello again'
+// console.log(sayHello);
+/*
+Number
+String
+Boolean
+Object
+Null
+Undefined
+*/ 
+// let huj=42
+// let string='Message'
+// let isTrue=true // false
+// let obj={a: 1}
+// let isNull=null
+// let undef=undefined
+
+// console.log(typeof undef)
+let num1=12
+let num2=8
+console.log('+', num1+num2)
+let str='Hello'
+let str2='World'
+console.log('+:', str+' '+str2)
+console.log('1+2', 1+'2')
+console.log('str+num1', str+num1)
